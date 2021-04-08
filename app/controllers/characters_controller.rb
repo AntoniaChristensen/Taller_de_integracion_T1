87e067c7f9f330e5be28535f7d1ed31e451ba898
AntoniaChristensen <achristensen1@uc.cl>
2021-04-08 18:29:44 -0400

    search bar

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -20,23 +20,36 @@ class CharactersController < ApplicationController
   end
 
   def index
-    @busqueda = search(params[:search])
+    search(params[:search])
+    if $search_characters.nil?
+      redirect_to root_path, alert: 'Character Not Found'
+    else
+      @busqueda = $search_characters
+    end
   end
 
   def search(s)
     if s
+      puts("aqui")
       name = params[:search].split(" ")
+      puts(name)
       ur = name.join("+")
+      puts(ur)
       url_characters = 'https://tarea-1-breaking-bad.herokuapp.com/api/characters/?name=%s' % ur
+      puts(url_characters)
       chars = HTTP.get(url_characters)
       if chars.status.success?
+        puts("aqui2")
         $search_characters = JSON.parse(chars)
         if $search_characters.nil?
+          puts("aqui3")
           redirect_to root_path, alert: 'Character Not Found'
         end
       elsif chars.code == 429
+        puts("aqui4")
         redirect_to root_path, alert: 'Too Many Requests'
       else
+        puts("aqui5")
         redirect_to root_path, alert: 'Character Not Found'
       end
     end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'characters/index'
   get 'characters/:name', to: 'characters#show', as: 'character', name: %r{([^/])+?}
   get 'characters/', to: 'characters#index', as: 'characters'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
